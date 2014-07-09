a82063aa9ec764561f0227803ce80d0fa7b4586e
和佐州洋 <kunihiro.wasa@gmail.com>
2014-07-09 11:34:42 +0900

    Fix all bugs

--- main.cpp
+++ main.cpp
@@ -16,20 +16,19 @@ class CircuitFindingAlgorithm
     std::map<Node, bool> blocked; 
     std::stack<Node> __stack; 
     Node s; 
-    Node __dummy_node = -1;
+    Node __dummy_node = 0;
     int __cnt; 
 
     void UNBLOCK(Node u);  
     bool CIRCUIT(Node v); 
     void OUTPUT(); 
     void SET_Ak(Graph & __g, std::map<Node, Node> & I); 
-    void DFS(Node v, Node & c, std::stack<Node> & S, std::stack<Node> & B,  std::map<Node, Node> & I, Graph & __g); 
     Node LEAST_VERTEX_Ak(); 
     Node MIN_VERTEX(Graph & __g); 
     Node MAX_VERTEX(Graph & __g); 
 
-    void __scc(Graph & __g, std::map<Node, Node> & I); 
-    Node __ith_on_stack(int i, std::stack<Node> & s); 
+    void __scc(Graph & __g, std::map<Node, int> & I); 
+    void DFS(Node v, int & c, std::stack<Node> & S, std::stack<int> & B,  std::map<Node, int> & I, Graph & __g); 
 
     public:
         void MAIN(Graph & __g); 
@@ -39,10 +38,10 @@ class CircuitFindingAlgorithm
 int main(int argc, char const* argv[])
 {
     Graph g = {
-        {0, {1, 2, 3}}, 
-        {1, {0, 2, 3}},     
-        {2, {0, 1, 3}},    
-        {3, {0, 1, 2}}    
+        {1, {2, 3, 4}}, 
+        {2, {1, 3, 4}},     
+        {3, {1, 2, 4}},    
+        {4, {1, 2, 3}}    
     }; 
     CircuitFindingAlgorithm CFA; 
 
@@ -56,8 +55,11 @@ void CircuitFindingAlgorithm::MAIN(Graph & __g)
 {
     Node n = MAX_VERTEX(__g);
          s = MIN_VERTEX(__g);
-    std::map<Node, Node> I; 
+    std::map<Node, int> I; 
     __scc(__g, I); 
+    // for (auto& ii : I) {
+        // std::cout << ii.first << ", " << ii.second << std::endl; 
+    // }
     __cnt = 0; 
     while (s < n) {
         SET_Ak(__g, I); 
@@ -139,85 +141,66 @@ void CircuitFindingAlgorithm::SET_Ak(Graph & __g, std::map<Node, Node> & I)
     std::vector<Node> Vk; 
     Ak.clear(); 
     for (auto&& v: I) {
-        if (s <= v.first) {
-            Vk.push_back(v.first); 
+        if (v.second == I[s] and v.first >= s) {
             Ak[v.first] = {}; 
-        }
-    }
-    for (auto&& v : __g) {
-        if (v.first != I[s]) {
-            continue; 
-        }
-        for (auto&& u : v.second) {
-            if (Vk.end() != std::find(Vk.begin(), Vk.end(), u) and u == I[s]) {
-                Ak[v.first].push_back(u); 
+            for (auto& w : __g[v.first]) {
+                if (I[s] == I[w] and w >= s) {
+                    Ak[v.first].push_back(w); 
+                }
             }
         }
     }
+    // for (auto& ak : Ak) {
+        // std::cout << ak.first << ": "; 
+        // for (auto& v : ak.second) {
+            // std::cout << v << " "; 
+        // }
+        // std::cout << std::endl; 
+    // }
+    // std::cout << std::endl; 
 }
 
 
 // Gabow, IPL, 2000. 
-void CircuitFindingAlgorithm::__scc(Graph & __g, std::map<Node, Node> & I) 
+void CircuitFindingAlgorithm::__scc(Graph & __g, std::map<Node, int> & I) 
 {
-    std::stack<Node> S, B; 
-    for (auto& cv : __g) {
-        I[cv.first] = __dummy_node; 
+    std::stack<Node> S; 
+    std::stack<int> B; 
+    for (auto& v : __g) {
+        I[v.first] = 0; 
     }
-    Node c = __dummy_node; // (int) __g.size(); 
-    for (auto&& v : __g) {
-        if (I[v.first] == __dummy_node) {
+    int c = (int) __g.size(); 
+    for (auto& v : __g) {
+        if (I[v.first] == 0) {
             DFS(v.first, c, S, B, I, __g); 
         }
     }
 }
     
-void CircuitFindingAlgorithm::DFS(Node v, Node & c, std::stack<Node> & S, std::stack<Node> & B,  std::map<Node, Node> & I, Graph & __g) 
+void CircuitFindingAlgorithm::DFS(Node v, int & c, std::stack<Node> & S, std::stack<int> & B,  std::map<Node, int> & I, Graph & __g) 
 {
     S.push(v); 
-    I[v] = S.top(); 
+    I[v] = S.size(); 
     B.push(I[v]); 
-    for (auto&& w : __g[v]) {
-        if (I[w] == __dummy_node) {
+    for (auto& w : __g[v]) {
+        if (I[w] == 0) {
             DFS(w, c, S, B, I, __g); 
         } else {
-            while (I[v] < __ith_on_stack(B.top(), B)) {
+            while (I[w] < B.top()) {
                 B.pop(); 
             }
         }
     }
 
-    if (I[v] == __ith_on_stack(B.top(), B)) {
+    if (I[v] == B.top()) {
         B.pop(); ++c; 
-        while (I[v] <= S.top()) {
+        while (I[v] <= S.size()) {
             I[S.top()] = c; 
             S.pop(); 
         }
     }
 }
 
-Node CircuitFindingAlgorithm::__ith_on_stack(int i, std::stack<Node> & s)
-{
-    Node ith_node = __dummy_node; 
-    if (s.empty()) {
-        return ith_node; 
-    }
-    std::stack<Node> __dummy_stack; 
-    for (; 0 < i and !s.empty(); --i) {
-        __dummy_stack.push(s.top()); 
-        s.pop(); 
-    }
-    
-    if (!s.empty()) {
-        ith_node = s.top(); 
-    }
-    while (!__dummy_stack.empty()) {
-        s.push(__dummy_stack.top()); 
-        __dummy_stack.pop(); 
-    }
-
-    return ith_node; 
-}
 
 Node CircuitFindingAlgorithm::LEAST_VERTEX_Ak()
 {
